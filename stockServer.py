2d87fd0ddc0ba35934b897239eec711111782989
danewest <cameron.west676@topper.wku.edu>
2025-04-06 23:18:12 -0500

    Listens for client, once client reaches it sends a welcome message.

--- stockServer.py
+++ stockServer.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import psycopg2
+from database import get_stock_price
 
 HOST = '0.0.0.0'
 PORT = 65432
@@ -16,4 +17,28 @@ connection = psycopg2.connect(
 
 def handle_client(client_socket, address):
     print(f"[+] New connection from {address}")
-    client_socket.sendall(b"Welcome to Stock Notifier!\n")
+    client_socket.sendall(b"Welcome to PriceQuake!\n")
+
+    # Example interaction -> Sending stock data
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT symbol, price FROM stocks LIMIT 5;")
+    for symbol, price in cursor.fetchall():
+        message = f"{symbol}: ${price}\n"
+        client_socket.sendall(message.encode())
+
+    client_socket.close()
+
+def start_server():
+    print(f"[*] Starting server on {HOST}:{PORT}")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+        client_thread.start()
+
+if __name__ == "__main__":
+    start_server()
