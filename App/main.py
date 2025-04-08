9711dcbe87afd7ca513a50ecdfe6b11b0288b007
danewest <cameron.west676@topper.wku.edu>
2025-04-07 22:00:54 -0500

    Learning to use FastAPI

--- App/main.py
+++ App/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from App.api.routes import router as api_router
+inport Sockets.stockClient
+import Sockets.stockServer
 
 app = FastAPI()
-
 app.include_router(api_router)
 
+@app.get("/")
+def root():
+    blahblahablah
