49fe6baa7416bff6f51832786c1ec59b9dd526a3
danewest <cameron.west676@topper.wku.edu>
2025-11-10 23:37:58 -0600

    Tweaks to the dashboard

--- frontend/pricequake-dashboard/src/app/stock-table/stock-table.ts
+++ frontend/pricequake-dashboard/src/app/stock-table/stock-table.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration } from 'chart.js';
 @Component({
   selector: 'app-stock-table',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, BaseChartDirective],
   templateUrl: './stock-table.html',
   styleUrl: './stock-table.scss',
 })
 export class StockTable {
+  lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: ['9:00', '9:05', '9:10', '9:15', '9:20'],
+    datasets: [
+      {
+        data: [195.1, 195.3, 195.0, 194.9, 195.2],
+        label: 'AAPL Stock',
+        fill: false,
+        tension: 0.3,
+        borderColor: '#5c00d4',
+        pointBackgroundColor: 'rgba(0,120,212,0.2)',
+        pointRadius: 3,
+        borderWidth: 2
+      }
+    ]
+  };
 
+  lineChartOptions: ChartConfiguration<'line'>['options'] = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
 }
--- frontend/pricequake-dashboard/src/main.ts
+++ frontend/pricequake-dashboard/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+// import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
